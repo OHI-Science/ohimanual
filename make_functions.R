c9bfdfe3935172805ef1835b267feb36e8fa993e
Ning Jiang <mendes@nceas.ucsb.edu>
2016-02-03 14:39:51 -0800

    modified make_fucntions.R to push html to ohi-sci.org.io

--- make_functions.R
+++ make_functions.R
@@ -48,28 +48,8 @@ ohi_docx = function(out_md) {
 }
 
 # render html for OHI in local ohimanual repo---- 
-ohi_html_local = function(out_md) {
+ohi_html = function(out_md) {
   cat('creating local .html file...\n')
-  render(
-    out_md,
-    html_document(
-      number_sections=T, fig_width = 3, fig_height = 2, fig_retina = 2, fig_caption = T, smart=T,
-      self_contained=F, theme='default',
-      highlight='default', mathjax='default', template='default',
-      toc=T, toc_depth=3),
-    clean=T, quiet=F,
-    output_file = paste0(pfx, '.html'))
-}
-
-
-# render html for OHI and push to ohi-science.org
-ohi_html = function(out_md, title_header, title_short) { 
-  cat('creating ohi-science.org .html file...\n')
-  
-  ## checkout and pull dev branch: ohi-science.github.io
- system('cd ~/github/ohi-science.github.io; git checkout dev; git pull')
-  
-  ## render html
   render(
     out_md,
     html_document(
@@ -78,46 +58,40 @@ ohi_html = function(out_md, title_header, title_short) {
       highlight=NULL, mathjax='default',
       toc=T, toc_depth=3),
     clean=T, quiet=F,
-    output_file = paste0(pfx, '-external.html'))
-  
-  ## prepend required header
-  cat(sprintf('---
-layout: manual
-title: %s
-subtitle: %s
-tagline: %s
----
-{%s include JB/setup %s}
-', 
-              title_header,
-              subtitle_header,
-              format(Sys.time(), "%d %B %Y"), '%', '%'), 
-      file=sprintf('~/github/ohi-science.github.io/%s/index.html', tolower(title_short)))
-  
-  ## save
-  cat(
-    readLines(paste0(pfx, '-external.html')),
-    file=sprintf('~/github/ohi-science.github.io/%s/index.html', tolower(title_short)), append=T)
-  if (title_short == 'Manual') {
-    dir.create(sprintf('~/github/ohi-science.github.io/%s/fig', tolower(title_short)), showWarnings=F)
-    file.copy('fig', sprintf('~/github/ohi-science.github.io/%s', tolower(title_short)), overwrite=T, recursive=T)
-  }
-  
-  ## commit and push to dev branch: ohi-science.github.io
-  system(sprintf(
-    'cd ~/github/ohi-science.github.io; 
-    git checkout dev; 
-    git pull; 
-    git add %s -A; 
-    git commit -m "updates pushed from ohimanual/make_%s.r"; 
-    git push', 
-    tolower(title_short), tolower(title_short)))
-  
-  message(sprintf('\nohi-science.org/%s was updated\n', tolower(title_short)))
-
+    output_file = paste0(pfx, '.html'))
 }
 
-# little copy_archive to copy 
+
+  ## commit and push to master branch: ohi-science.github.io
+push_to_web = function(out_md) {
+  
+  # read filename
+  fn = str_split(out_md, "\\.");
+  f  = fn[[1]][1]
+  
+  # copy .html file to ohi-science.github.io
+  file.copy(sprintf('%s.html', f), sprintf('~/github/ohi-science.github.io/%s', tolower(title_short)), overwrite=T, recursive=T)
+  
+  if (f == 'ohi-manual') {
+    dir.create('~/github/ohi-science.github.io/_includes/themes/OHI/manual', showWarnings=F)
+    file.copy('fig', '~/github/ohi-science.github.io/_includes/themes/OHI/manual', overwrite=T, recursive=T)
+  }
+  
+  # git push ohi-science.github.io
+  system(sprintf(
+      'cd ~/github/ohi-science.github.io; 
+    git checkout master; 
+    git pull; 
+    git add %s.html -A; 
+    git commit -m "%s.html pushed from ohimanual/make_*.r"; 
+    git push', 
+    f, f))
+  
+  message(sprintf('\nohi-science.org/%s was updated\n', f))
+  
+}
+
+# little copy_archive to copy pdf files
 copy_archive = function(dir_fn,
                         path_in  = '~/github/ohimanual',
                         path_out = '~/github/ohi-science.github.io/assets/downloads/other') { 
